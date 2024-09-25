f62bf7622a38065c3dfca8420d0c2ee3fb9167c8
DDING <ddingsha9@teambind.co.kr>
2024-09-25 12:44:38 +0900

    fix : ..

--- src/main/java/com/example/graduation/Profile/profileController/ProfileController.java
+++ src/main/java/com/example/graduation/Profile/profileController/ProfileController.java
@@ -13,8 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Optional;
-
 
 @Api(value = "Profile Controller", tags = "Profile ")
 @RequiredArgsConstructor
